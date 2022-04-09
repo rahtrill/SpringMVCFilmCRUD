994bc3698521ed5c32411361ed1ebc1d10272e4c
Rahil Ali <rahtrill@gmail.com>
2022-04-09 11:49:52 -0600

    Initial ID html and some controller work

--- src/main/java/com/skilldistillery/mvcfilmsite/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/mvcfilmsite/controllers/FilmController.java
@@ -3,6 +3,8 @@ package com.skilldistillery.mvcfilmsite.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.mvcfilmsite.data.FilmDAO;
 
@@ -11,9 +13,18 @@ public class FilmController {
 
 	@Autowired
 	private FilmDAO filmDao;
-	
-	@RequestMapping(path = {"/", "home.do"})
+
+	@RequestMapping(path = { "/", "home.do" })
 	public String home() {
-		return "WEB-INF/home.jsp";
+		return "WEB-INF/index.html";
+	}
+
+	@RequestMapping(path = "findByID.do", method = RequestMethod.GET)
+	public ModelAndView findByID(String id) {
+		ModelAndView mv = new ModelAndView();
+
+		mv.setViewName("NewStateResult");
+
+		return mv;
 	}
 }
